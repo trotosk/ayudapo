36b7e5afeaff1be56f5aacc9adf50e419e85fd14
trotosk <38053758+trotosk@users.noreply.github.com>
2025-09-01 16:46:17 +0200

    Update templates.py

--- templates.py
+++ templates.py
@@ -31,6 +31,19 @@ def get_criterios_epica_template():
     El detalle es: {input}
     """
 
+def get_criterios_epica_only_history_template():
+    return """
+    Eres un experto Product owner experto de una aplicación informatica.
+    Realizas tareas de analisis y definicion para un cliente muy grande. 
+    Quiero detallar una epica con forma: titulo; Creemos que; para; conseguiremos.
+    Quiero tambien una descripcion de la epica.
+    Tambien mostrar los criterios de exito resumidos sin formato tabla y numerados de la forma C.E.1, C.E.2 y asi sucesivamente.
+    Se han de listar tambien posibles riesgos y dependencias si las hay.
+    Mostrar el detalle de una historia para la construccion y pruebas de la epica con formato: titulo; Como; QUiero; Para; detalle.
+    Para cada una de estas historias han de mostrarse los casos de uso con el formato: dado, cuando y entonces. Se han de mostrar en una tabla de 3 columnas.
+    El detalle de la epica es: {input}
+    """
+
 def get_criterios_mejora_template():
     return """
     Eres un experto Product owner experto de una aplicación informatica.
@@ -65,3 +78,4 @@ def get_resumen_reunion_template():
     Haces mucha reuniones y necesitas sacar el resumen de la reunion, con participantes, resumen con puntos mas importantes y un resumen de cada punto, asi como las epicas que pueden crearse de lo que se hable.
     El detalle de la reunion y la transcripcion es: {input}
     """
+
